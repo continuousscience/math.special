fb4097adfe3d8c8ad844f9d7f4f814cd50faae72
David M. Rogers <predictivestatmech@gmail.com>
2017-08-06 22:55:17 -0400

    Added / swapped some functions.

--- src.c
+++ src.c
@@ -1,10 +1,11 @@
-#include <math.h>
 #include <dlfcn.h>
 #include <sil_ext.h>
 
 typedef double (*unop)(double);
 typedef double (*nunop)(int, double);
 typedef double (*binop)(double, double);
+typedef int (*intfop)(double);
+typedef int (*intop)(int);
 
 #define math_unop(_op) \
 int _op(sil_State *S) { \
@@ -35,6 +36,25 @@ int _op(sil_State *S) { \
     return 0; \
 }
 
+#define math_intfop(_op) \
+int _op(sil_State *S) { \
+    double x = sil_todouble(S, 1); \
+    intfop real_op = (intfop)dlsym(RTLD_NEXT, #_op); \
+    sil_pop(S, 1); \
+    sil_pushinteger(S, real_op(x)); \
+    return 0; \
+}
+
+// fake integer operation
+#define math_fintop(_op) \
+int _op(sil_State *S) { \
+    double x = sil_todouble(S, 1); \
+    unop real_op = (unop)dlsym(RTLD_NEXT, #_op); \
+    sil_pop(S, 1); \
+    sil_pushinteger(S, (int)real_op(x)); \
+    return 0; \
+}
+
 // create unary operations
 math_unop(sin);
 math_unop(cos);
@@ -50,8 +70,7 @@ math_unop(acosh);
 math_unop(atanh);
 math_unop(exp);
 math_unop(exp2);
-math_unop(exp10);
-math_unop(pow1);
+//math_unop(exp10);
 math_unop(log);
 math_unop(log10);
 math_unop(log2);
@@ -66,6 +85,12 @@ math_unop(j0);
 math_unop(j1);
 math_unop(y0);
 math_unop(y1);
+math_unop(fabs);
+
+math_intfop(ilogb);
+math_fintop(floor);
+math_fintop(ceil);
+math_fintop(round);
 
 // create int, double operations
 math_nunop(jn);
@@ -95,3 +120,22 @@ int lgamma(sil_State *S) {
     sil_pushdouble(S, real_op(x, &signp));
     return 0;
 }
+
+int mod(sil_State *S) {
+    int a = sil_tointeger(S, 1);
+    int b = sil_tointeger(S, 2);
+    sil_pop(S, 2);
+    sil_pushinteger(S, ((a%b)+b) % b);
+    return 0;
+}
+
+int fmod(sil_State *S) {
+    double x = sil_todouble(S, 1);
+    double y = sil_todouble(S, 2);
+    binop real_op = (binop)dlsym(RTLD_NEXT, "fmod");
+    sil_pop(S, 2);
+    sil_pushdouble(S, real_op(real_op(x, y)+y, y));
+    return 0;
+}
+
+
